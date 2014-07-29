e9435855044269b67c37e9f1c1765966582e5e48
gideong <gideong@yahoo-inc.com>
2014-07-29 10:23:25 -0700

    Adding pages.

--- client/router.js
+++ client/router.js
@@ -1,12 +1,34 @@
 var Router = require('ampersand-router');
 var querystring = require('querystring');
 var HomePage = require('./pages/home');
+var PatternPage = require('./pages/pattern');
+var PoolPage = require('./pages/pool');
+var SetPage = require('./pages/set');
+var SongPage = require('./pages/song');
+
 module.exports = Router.extend({
 	routes: {
-		'': 'home'
+		'': 'home',
+		':set': 'set',
+		':set/pattern/:pattern': 'pattern',
+		':set/pool': 'pool',
+		':set/song': 'song'
 	},
 
 	home: function() {
 		this.trigger('page', new HomePage());
+	},
+	set: function(setName) {
+		debugger;
+		this.trigger('page', new SetPage(setName));
+	},
+	pattern: function(setName, patternName) {
+		this.trigger('page', new PatternPage(setName, patternName));
+	},
+	pool: function(setName) {
+		this.trigger('page', new PoolPage(setName));
+	},
+	song: function(setName) {
+		this.trigger('page', new SongPage(setName));
 	}
 });
