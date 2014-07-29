81be139efbafb3c2aa8d29e03761a541f63e71d7
gideong <gideong@yahoo-inc.com>
2014-07-29 00:28:17 -0700

    Less

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+//var path = require('path');
+
+// TODO gideong: add a default task, add a watch task, figure out how to make this more automatic.
+//  see http://markgoodyear.com/2014/01/getting-started-with-gulp/
+
+gulp.task('less', function() {
+	gulp.src('./less/main.less')
+		.pipe(less({
+			//paths: [path.join(__dirname, 'less', 'includes')]
+		}))
+		.pipe(gulp.dest('./dist/css'));
+});
--- server.js
+++ server.js
@@ -11,10 +11,9 @@ server.pack.register({
 		moonboots: {
 			main: __dirname + '/client/app.js',
 			developmentMode: config.isDev,
-			// stylesheets: [
-			// 	__dirname + '/public/css/bootstrap.css',
-			// 	__dirname + '/public/css/main.css'
-			// ],
+			stylesheets: [
+				__dirname + '/dist/css/main.css'
+			],
 			beforeBuildJS: function() {
 				if (config.isDev) {
 					templatizer(__dirname + '/templates', __dirname + '/client/templates.js');
