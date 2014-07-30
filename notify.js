6621c3c6d42e105b2b96ee8341ec251204627f8d
ggoodwin37 <ggoodwin37@gmail.com>
2014-07-29 21:16:09 -0700

    Getting server code ready for api. Adding db.

---
+++ notify.js
@@ -0,0 +1,75 @@
+//var Stream = require('stream');
+//var _ = require('underscore');
+
+//var Tweets = require('./models/tweet');
+//var User = require('./models/user');
+//var WSS = require('ws').Server;
+
+module.exports.register = function (plugin, options, next) {
+// TODO: figure out ws requirements. primus?
+/*
+    var wss = new WSS({ server: plugin.servers[0].listener });
+    wss.broadcast = function (data) {
+        this.clients.forEach(function (client) {
+            client.send(data);
+        });
+    };
+
+    var routes = [];
+    var channelNameForUser = function (userId, channelType, done) {
+        User.get(userId, function (err, user) {
+            if (err) console.error(err);
+
+            var channel = options.publicUrl + '/wolves/' + user.username + '/' + channelType;
+            done(null, channel);
+        });
+    };
+
+    var sendNotification = function (data) {
+        wss.broadcast(JSON.stringify(data));
+    };
+
+    var pushHowl = function (model) {
+        sendNotification({
+            action: 'update',
+            channel: options.publicUrl + '/howls',
+            url: options.publicUrl + '/howls/' + model.id,
+            id: model.id
+        });
+    };
+
+    var pushUserHowl = function (model) {
+        channelNameForUser(model.user, 'howls', function (err, channelName) {
+            if (err) return console.log(err);
+
+            sendNotification({
+                action: 'update',
+                channel: channelName,
+                url: options.publicUrl + '/howls/' + model.id,
+                id: model.id
+            });
+        });
+    };
+
+    var pushUserMarks = function (model) {
+        _.flatten(model.mentions).forEach(function (userId) {
+            channelNameForUser(userId, 'marks', function (err, channelName) {
+                sendNotification({
+                    action: 'update',
+                    channel: channelName,
+                    url: options.publicUrl + '/howls' + model.id,
+                    id: model.id
+                });
+            });
+        });
+    };
+
+    Tweets.events.on('save', function (model) {
+        pushHowl(model);
+        pushUserHowl(model);
+        pushUserMarks(model);
+    });
+
+    next();
+*/
+};
--- server.js
+++ server.js
@@ -1,28 +1,84 @@
 var hapi = require('hapi');
-var moonboots = require('moonboots_hapi');
 var config = require('getconfig');
-var templatizer = require('templatizer');
 
-var server = hapi.createServer(config.serverPort, config.serverHost);
-server.pack.register({
-	plugin: moonboots,
+var server = new hapi.Server(config.serverPort, config.serverHost, {
+    // not using any server views right now.
+    // views: {
+    //     engines: { jade: require('jade') },
+    //     path: __dirname + '/templates'
+    // },
+
+    // haven't tested cors yet, can
+    // cors: {
+    //     origin: ['*'],
+    //     headers: ['Authorization', 'Content-Type', 'If-None-Match', 'Auth-Token']
+    // }
+});
+
+var resources = {
+    // TODO: resources here.
+    // wolves: require('./resources/users'),
+    // howls: require('./resources/tweets')
+};
+
+var dulcimer = require('dulcimer');
+dulcimer.connect({
+    type: 'level',
+    path: __dirname + '/db',
+    bucket: 'TODO' // TODO
+});
+
+// TODO: models
+// var Tweet = require('./models/tweet');
+// var User = require('./models/user');
+
+server.pack.register(require('lout'), function () {});
+
+server.pack.register([
+    {
+        plugin: require('mudskipper'),
+        options: { resources: resources }
+    },
+    // {
+    //     plugin: require('./notify'),
+    //     options: { publicUrl: 'http://' + config.serverHost + ':' + config.serverPort }
+    // },
+    {
+	plugin: require('moonboots_hapi'),
 	options: {
-		appPath: '/{p*}',
-		moonboots: {
-			main: __dirname + '/client/app.js',
-			developmentMode: config.isDev,
-			stylesheets: [
-				__dirname + '/dist/css/main.css'
-			],
-			beforeBuildJS: function() {
-				if (config.isDev) {
-					templatizer(__dirname + '/templates', __dirname + '/client/templates.js');
-				}
+	    appPath: '/{p*}',
+	    moonboots: {
+		main: __dirname + '/client/app.js',
+		developmentMode: config.isDev,
+		stylesheets: [
+		    __dirname + '/dist/css/main.css'
+		],
+		beforeBuildJS: function() {
+		    if (config.isDev) {
+			var templatizer = require('templatizer');
+			templatizer(__dirname + '/templates', __dirname + '/client/templates.js');
+		    }
 
-			}
 		}
+	    }
 	}
-}, function() {
-	server.start();
-	console.log('rhythm-ninja is running at ' + config.serverHost + ':' + config.serverPort);
+    }
+], function (err) {
+    if (err) throw err;
+
+    // TODO: what server routes do we need here? api/mudskipper?
+    // server.route(require('./resources/client')());
+    // server.route({
+    //     method: 'GET',
+    //     path: '/public/{param*}',
+    //     handler: {
+    //         directory: {
+    //             path: 'public'
+    //         }
+    //     }
+    // });
+
+    server.start(function () {
+        console.log('rhythm-ninja is running at', server.info.uri);
+    });
 });
