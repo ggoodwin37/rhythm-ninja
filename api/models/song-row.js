153382ed77c979ac515cf9db4689db7a620125cd
gideong <gideong@yahoo-inc.com>
2014-08-14 22:05:59 -0700

    Whoopsie

--- api/models/song-row.js
+++ api/models/song-row.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var modelName = 'song-row';
 var schema = mongoose.Schema({
 	parent_id: mongoose.Schema.Types.ObjectId,
-	pattern_id: mongoose.Schema.Types.ObjectId
+	pattern_id: mongoose.Schema.Types.ObjectId,
 	offset: Number,
 	len: Number,
 	count: Number
