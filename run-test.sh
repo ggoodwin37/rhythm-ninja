48d544b995fadedaa193a8075bc796fcd214be35
ggoodwin37 <ggoodwin37@gmail.com>
2014-08-16 12:56:35 -0700

    Test flag no longer needed (shrug)

--- run-test.sh
+++ run-test.sh
@@ -3,5 +3,4 @@
 # tell getconfig where to find config
 export GETCONFIG_ROOT=`pwd`
 
-# passing -l here to avoid (buggy?) global leak detection
-node_modules/lab/bin/lab -v -l test/index.js
+node_modules/lab/bin/lab -v test/index.js
