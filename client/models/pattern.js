979a12b16fa04064231d0833fa7df5476fb78ca3
ggoodwin37 <ggoodwin37@gmail.com>
2015-01-19 15:54:34 -0800

    Pattern models.

---
+++ client/models/pattern.js
@@ -0,0 +1,19 @@
+var AmpModel = require('ampersand-model');
+
+module.exports = AmpModel.extend({
+    props: {
+        id: ['string'],
+        length: ['number'],
+        locked: ['boolean'],
+        name: ['string'],
+        parent_id: ['string'],
+        rows: ['array']
+    },
+	initialize: function(params) {
+		this.setName = params.setName;
+		this.id = params.patternId;
+	},
+	url: function() {
+		return '/api/set/' + this.setName + '/pattern/' + this.id;
+	}
+});
---
+++ client/models/patterns.js
@@ -0,0 +1,7 @@
+var AmpCollection = require('ampersand-rest-collection');
+var Pattern = require('./pattern');
+
+module.exports = AmpCollection.extend({
+    model: Pattern,
+    url: '/api/pattern'
+});
