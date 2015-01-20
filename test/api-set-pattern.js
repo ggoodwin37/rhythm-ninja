6c4d4ce67f3572e6c7aa5c9c6fbbb0dbde0c52ec
ggoodwin37 <ggoodwin37@gmail.com>
2015-01-19 20:47:25 -0800

    Actually this scenario is handled by deep population.

--- test/api-set-pattern.js
+++ test/api-set-pattern.js
@@ -10,7 +10,7 @@ module.exports = function(ctx) {
 	var describe = Lab.experiment;
 	var it = Lab.test;
 
-	describe('verify set/:setId/patterns api (retrieve all patterns belonging to set)', function() {
+	describe('verify set deep population', function() {
 
 		it('should delete any existing documents with the test name', function(done) {
 			ctx.server.inject({
@@ -58,26 +58,24 @@ module.exports = function(ctx) {
 			});
 		});
 
+		var resSet;
 		it('should have added the new patterns to the set for me', function(done) {
 			ctx.server.inject({method: 'get', url: ctx.baseSetUrl}, function(res) {
 				expect(res.statusCode).to.equal(200);
-				expect(res.result.patterns.length).to.equal(patternNames.length);
+				resSet = res.result;
+				expect(resSet.patterns.length).to.equal(patternNames.length);
 				done();
 			});
 		});
 
-		it('should give me all patterns for the set', function(done) {
-			var patternsForSetUrl = ctx.baseSetUrl + '/patterns';
-			ctx.server.inject({method: 'get', url: patternsForSetUrl}, function(res) {
-				expect(res.statusCode).to.equal(200);
-				var patts = res.result.patterns;
-				expect(patts.length).to.equal(patternNames.length);
-				patts.forEach(function(patt) {
-					expect(patt.length).to.equal(8);
-					expect(patternNames.some(function(name) { return name === patt.name; } )).to.equal(true);
-				});
-				done();
+		it('should have populated result set', function(done) {
+			resSet.patterns.forEach(function(pattern) {
+				expect(patternNames.some(function(name) {
+					return name === pattern.name;
+				})).to.equal(true);
+				expect(pattern.length).to.equal(8);
 			});
+			done();
 		});
 
 	});
