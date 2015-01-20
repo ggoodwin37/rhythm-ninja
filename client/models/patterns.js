c19a6bdbdab0f53e29862ce4977d91076f9efd0d
ggoodwin37 <ggoodwin37@gmail.com>
2015-01-19 20:52:17 -0800

    Minor backtracking.

--- client/models/patterns.js
+++ client/models/patterns.js
@@ -2,13 +2,5 @@ var AmpCollection = require('ampersand-rest-collection');
 var Pattern = require('./pattern');
 
 module.exports = AmpCollection.extend({
-    model: Pattern,
-	initialize: function(params) {
-		console.log('init patterns collection with params:', params);
-		this.setName = (params || {}).setName;
-	},
-	url: function() {
-		// TODO: I think this endpoint doesn't exist, although it's in the doc.
-		return '/api/set/' + this.setName + '/pattern/';
-	}
+    model: Pattern
 });
--- client/pages/set.js
+++ client/pages/set.js
@@ -5,7 +5,6 @@ var PatternsView = require('../views/set/patterns.js');
 var SongsView = require('../views/set/songs.js');
 
 var Set = require('../models/set');
-var Patterns = require('../models/patterns');
 
 module.exports = View.extend({
 	template: templates.pages.set(),
@@ -33,15 +32,6 @@ module.exports = View.extend({
 		this.model.fetch({
 			success: function(model, response) {
 				console.log('successfully fetched a set:', model);
-				var patterns = new Patterns(self.params);
-				patterns.fetch({
-					success: function(patternModels, response) {
-						console.log('successfully fetched set patterns:', patternModels);
-					},
-					error: function(patternModels, response) {
-						console.log('error fetching set patterns:', response);
-					}
-				});
 			},
 			error: function(model, response) {
 				console.log('error fetching set:', response);
