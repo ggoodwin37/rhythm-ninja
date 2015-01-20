6cbdc51295b80df9b7b30bbc4725c4a55c3b4292
ggoodwin37 <ggoodwin37@gmail.com>
2015-01-19 21:44:48 -0800

    adding a resume note. and some minor cleanup.

--- client/pages/set.js
+++ client/pages/set.js
@@ -7,15 +7,25 @@ var SongsView = require('../views/set/songs.js');
 var Set = require('../models/set');
 
 module.exports = View.extend({
-	template: templates.pages.set(),
+	template: templates.pages.set,
 	subviews: {
 		pool: {
 			constructor: PoolView,
 			role: 'pool'
 		},
 		patterns: {
-			constructor: PatternsView,
-			role: 'patterns'
+			container: '[role=patterns]',
+			// TODO: resume here. trying to figure out how to hook up the rehydrated patterns with
+			//  the pattern subview. this waitFor trigger is breaking somehow. would prefer to wait
+			//  for the whole model, but I think that means I have to instantiate this.model differently
+			//  than I'm doing now in initialize()
+			// waitFor: 'model.patterns',
+			prepareView: function(el) {
+				return new PatternsView({
+					el: el,
+					model: this.model.patterns
+				});
+			}
 		},
 		songs: {
 			constructor: SongsView,
@@ -31,7 +41,7 @@ module.exports = View.extend({
 		this.model = new Set(this.params);
 		this.model.fetch({
 			success: function(model, response) {
-				console.log('successfully fetched a set:', model);
+				console.log('fetched set:', model);
 			},
 			error: function(model, response) {
 				console.log('error fetching set:', response);
--- client/views/set/patterns.js
+++ client/views/set/patterns.js
@@ -3,8 +3,14 @@ var templates = require('../../templates');
 
 module.exports = View.extend({
 	template: templates.includes.set.patterns,
-	bindings: {
-		'model.foo': 'bar-role'
+	// bindings: {
+	// 	'model.foo': 'bar-role'
+	// },
+	initialize: function(params) {
+		// TODO: need model collection/array here, or else need to listen for when it gets set.
+		//  I wouldn't mind listener approach, that way we can render ourselves in loading state
+		//  before api call is done.
+		console.log('patterns subview initialized', params);
 	},
 	render: function() {
 		this.renderWithTemplate();
