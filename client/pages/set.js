84f930e5258de0c8c0c7c82d396d93ad91dc8fde
ggoodwin37 <ggoodwin37@gmail.com>
2015-01-24 14:48:51 -0800

    wip

--- client/pages/set.js
+++ client/pages/set.js
@@ -17,10 +17,11 @@ module.exports = View.extend({
 		patterns: {
 			container: '[role=patterns]',
 			prepareView: function(el) {
+				console.log('PREP PATTERN SUBVIEW');
 				var self = this;
 				var subview = new PatternsView({
 					el: el,
-					model: this.model.patterns
+					model: this.model
 				});
 				this.on('model-loaded', function(model) {
 					console.log('listener firing with model:', model);
@@ -39,12 +40,14 @@ module.exports = View.extend({
 		'model.name': 'setName'
 	},
 	initialize: function(params) {
+		console.log('INIT SET PAGE');
 		var self = this;
 		this.params = params || {};
 		Events.createEmitter(this);
 		this.model = new Set(this.params);
 		this.model.fetch({
 			success: function(model, response) {
+				console.log('MODEL LOADED: ' + model.toJSON());
 				console.log('fetched set:', model);
 				self.trigger('model-loaded', model);
 			},
--- client/views/set/patterns.js
+++ client/views/set/patterns.js
@@ -7,15 +7,32 @@ module.exports = View.extend({
 	// 	'model.foo': 'bar-role'
 	// },
 	initialize: function(params) {
+		var self = this;
+
+		console.log('INIT SET->PATTERNS VIEW');
 		// TODO: need model collection/array here, or else need to listen for when it gets set.
 		//  I wouldn't mind listener approach, that way we can render ourselves in loading state
 		//  before api call is done.
 		console.log('patterns subview initialized', params);
+		this.model = params.model;
+		this.model.on('change:patterns', function() {
+			console.log('MODEL PATTERNS CHANGED', self.model.patterns);
+			self.render();
+		});
 	},
 	render: function() {
-		this.renderWithTemplate();
-		// this.renderSubview(new SomeSubView({
-		// 	model: this.model.get('some-sub-model')
-		// }), '[role=subViewRole]');
+		var patterns;
+		if (this.model && this.model.patterns) {
+			this.setLoading(false);
+			patterns = this.model.patterns;
+		} else {
+			this.setLoading(true);
+			patterns = [];
+		}
+		console.log('RENDERING WITH NUM PATTERNS=' + patterns.length);
+		this.renderWithTemplate({patterns: patterns});
+	},
+	setLoading: function(isLoading) {
+		console.log('setLoading:' + isLoading);
 	}
 });
