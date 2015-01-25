c148acbc81625ec82f961be415fc9c7fdd8be147
ggoodwin37 <ggoodwin37@gmail.com>
2015-01-24 20:44:22 -0800

    Add pool view.

--- client/views/set/pool.js
+++ client/views/set/pool.js
@@ -1,15 +1,35 @@
 var View = require('ampersand-view');
 var templates = require('../../templates');
+var dom = require('ampersand-dom');
 
 module.exports = View.extend({
 	template: templates.includes.set.pool,
-	bindings: {
-		'model.foo': 'bar-role'
+	initialize: function(params) {
+		var self = this;
+
+		this.model = params.model;
+		this.model.on('change:pool', function() {
+			self.render();
+		});
 	},
 	render: function() {
-		this.renderWithTemplate();
-		// this.renderSubview(new SomeSubView({
-		// 	model: this.model.get('some-sub-model')
-		// }), '[role=subViewRole]');
+		var pool = (this.model && this.model.pool) ? this.model.pool : null;
+		this.renderWithTemplate({
+			pool: pool,
+			setName: this.model.name,
+			slugger: function(input) {
+				return input.replace(' ', '-'); // TODO: better slugger
+			}
+		});
+		this.setLoading(!pool);
+	},
+	setLoading: function(isLoading) {
+		var el = this.queryByHook('pool-view-container');
+		var className = 'default-loading';
+		if (isLoading) {
+			dom.addClass(el, className);
+		} else {
+			dom.removeClass(el, className);
+		}
 	}
 });
