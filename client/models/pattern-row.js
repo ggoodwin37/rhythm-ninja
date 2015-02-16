7e1d8f0996ec4764b4c966ca606f27a38d46c3c6
ggoodwin37 <ggoodwin37@gmail.com>
2015-02-15 19:24:21 -0800

    WIP

--- client/models/pattern-row.js
+++ client/models/pattern-row.js
@@ -7,10 +7,16 @@ var PatternRowModel = AmpModel.extend({
 		steps: 'array'
 	},
 	initialize: function(patternRowData) {
+		// TODO: what's the least hacky way to get setName/patternId here?
+		this.setName = 'NEED_SET_NAME';
+		this.patternId = 'NEED_PATTERN_ID';
 		this.patternRowId = patternRowData.id;
 	},
 	url: function() {
-		var url = this.patternRowId;
+		var url = '/api/set/' + this.setName + '/pattern/' + this.patternId + '/patternrow';
+		if (this.patternRowId) {
+			url += '/' + this.patternRowId;
+		}
 		return url;
 	}
 });
