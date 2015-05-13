acaac6bd04e28c9af7569f334ddecb173182f495
ggoodwin37 <ggoodwin37@gmail.com>
2015-05-12 20:57:56 -0700

    Revert embedded doc change.

--- api/models/pattern.js
+++ api/models/pattern.js
@@ -1,5 +1,4 @@
 var mongoose = require('mongoose');
-var PatternRow = require('./pattern-row');
 
 var factory = null;
 module.exports = function(app) {
@@ -12,7 +11,7 @@ module.exports = function(app) {
 		name: String,
 		length: {type: Number, default: 16},
 		locked: Boolean,
-		rows: [PatternRow]  // breaks lots of tests. does this explain the [Object object] cast in generic save?
+		rows: [{type: String, ref: 'pattern-row'}]
 	});
 	require('./schema-id')(schema);
 	factory = mongoose.model(modelName, schema);
