0206567263e11ad3d83f4886406ceaa7e4e56df5
ggoodwin37 <ggoodwin37@gmail.com>
2015-05-23 09:16:45 -0700

    Extracting register-auth.

---
+++ register-auth.js
@@ -0,0 +1,49 @@
+function registerAuth(config) {
+	if (config.twitterAuth) {
+		var isSecure = false;  // TODO: enable isSecure (need to turn on https first)
+		server.auth.strategy('twitter', 'bell', {
+			provider: 'twitter',
+			password: config.cookieEncryptionPassword,
+			clientId: config.twitterAuth.clientId,
+			clientSecret: config.twitterAuth.clientSecret,
+			isSecure: isSecure
+		});
+		server.auth.strategy('session', 'cookie', {
+			password: config.cookiePassword,
+			cookie: 'sid',
+			redirectTo: '/login',
+			isSecure: isSecure
+		});
+		server.route({
+			method: ['GET', 'POST'],
+			path: '/login',
+			config: {
+				auth: 'twitter',
+				handler: function(request, reply) {
+					var twitterName = request.auth.credentials.profile.username;
+					var userInfo = {
+						rnUserKey: twitterName + '_twitter'
+					};
+					request.auth.session.set(userInfo);
+					console.log('user logged in:');
+					inspect(userInfo);
+					// TODO: need to apply session strategy to a test endpoint, and verify request.auth.session there.
+					return reply('<pre>' + JSON.stringify(userInfo) + '</pre>');
+				}
+			}
+		});
+		server.route({
+			method: 'POST',
+			path: '/logout',
+			handler: function(request, reply) {
+				if (request.auth && request.auth.session) {
+					request.auth.session.clear();
+					console.log('cleared session');
+				}
+				return reply('<h2>You are now logged out.</h2>');
+			}
+		});
+	}
+}
+
+module.exports = registerAuth;
--- server-instance.js
+++ server-instance.js
@@ -51,52 +51,7 @@ function startServerInstance(done) {
 	server.register(serverPluginList, function (err) {
 		if (err) throw err;
 
-		if (config.twitterAuth) {
-			var isSecure = false;  // TODO: enable isSecure (need to turn on https first)
-			server.auth.strategy('twitter', 'bell', {
-				provider: 'twitter',
-				password: config.cookieEncryptionPassword,
-				clientId: config.twitterAuth.clientId,
-				clientSecret: config.twitterAuth.clientSecret,
-				isSecure: isSecure
-			});
-			server.auth.strategy('session', 'cookie', {
-				password: config.cookiePassword,
-				cookie: 'sid',
-				redirectTo: '/login',
-				isSecure: isSecure
-			});
-			server.route({
-				method: ['GET', 'POST'],
-				path: '/login',
-				config: {
-					auth: 'twitter',
-					handler: function(request, reply) {
-						// TODO: extract and normalize this
-						var userInfo = {
-							id: request.auth.credentials.profile.id,
-							name: request.auth.credentials.profile.username
-						};
-						request.auth.session.set(userInfo);
-						console.log('user logged in:');
-						inspect(userInfo);
-						// TODO: need to apply session strategy to a test endpoint, and verify request.auth.session there.
-
-						// TODO: figure out what happens here, probably need hapi-auth-cookie hookup here.
-						return reply('<pre>' + JSON.stringify(request.auth.credentials, null, 4) + '</pre>');
-					}
-				}
-			});
-			// TODO: flesh this out a bit. not crazy about performing the clear on GET either.
-			server.route({
-				method: 'GET',
-				path: '/logout',
-				handler: function(request, reply) {
-					request.auth.session.clear();
-					return reply('<h2>You are now logged out.</h2>');
-				}
-			});
-		}
+		registerAuth(config);
 
 		server.start(function () {
 			console.log('rhythm-ninja is running at', server.info.uri);
