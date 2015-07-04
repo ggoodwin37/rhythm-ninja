2152daf1adee3efa6d42d94ca348adc8a03ec338
ggoodwin37 <ggoodwin37@gmail.com>
2015-07-03 21:33:03 -0700

    Auth refactor.

--- api/resources/generic-resource.js
+++ api/resources/generic-resource.js
@@ -10,6 +10,8 @@ var treeDelete = require('./tree-delete');
 
 module.exports = function(app, opts) {
 
+	var verifyAuth = require('./verify-auth')(app);
+
 	// alias into options
 	var parentQueryField = opts.parentQueryField;
 	var itemTypeName = opts.itemTypeName;
@@ -23,67 +25,95 @@ module.exports = function(app, opts) {
 	var populateFields = opts.populateFields;
 
 	return {
-		show: function(request, reply) {
-			var itemId = request.params[routeItemIdKey];
-			var query = itemFactory.findById(itemId);
-			if (populateFields) {
-				query.populate(populateFields);
-			}
-			var execQuery = function() {
-				query.exec(function(err, itemModel) {
-					if (handlingErrorOrMissing(err, itemModel, reply)) return;
-					return reply(itemModel.toJSON());
-				});
-			}
-			var fakeDelayMs = 0;
-			if (app.config.fakeDelayOnGetMs > 0) {
-				setTimeout(execQuery, app.config.fakeDelayOnGetMs);
-			} else {
-				execQuery();
+		show: {
+			handler: function(request, reply) {
+				if (!verifyAuth(request, reply)) return;
+
+				var itemId = request.params[routeItemIdKey];
+				var query = itemFactory.findById(itemId);
+				if (populateFields) {
+					query.populate(populateFields);
+				}
+				var execQuery = function() {
+					query.exec(function(err, itemModel) {
+						if (handlingErrorOrMissing(err, itemModel, reply)) return;
+						return reply(itemModel.toJSON());
+					});
+				}
+				var fakeDelayMs = 0;
+				if (app.config.fakeDelayOnGetMs > 0) {
+					setTimeout(execQuery, app.config.fakeDelayOnGetMs);
+				} else {
+					execQuery();
+				}
+			},
+			config: {
+				auth: app.authConfig
 			}
 		},
-		create: function(request, reply) {
-			var parentId = request.params[routeParentIdKey],
-				newModel;
-			function createAndSaveNewModel(cb) {
-				var newData = _.extend({parent_id: parentId}, request.payload);
-				newModel = new itemFactory(newData);
-				newModel.save(function(err, newModelResult) {
-					if (handlingError(err, reply)) return;
-					cb(null, newModelResult.id);
-				});
-			}
-			function updateParentModelCollection(newModelId, cb) {
-				var parentQuery = {};
-				parentQuery[parentQueryField] = parentId;
-				parentFactory.findOne(parentQuery, function(err, parentModel) {
-					if (handlingError(err, reply)) return;
-					parentModel[parentCollection].push(newModelId);
-					parentModel.save(function(err, parentModel) {
+		create: {
+			handler: function(request, reply) {
+				if (!verifyAuth(request, reply)) return;
+
+				var parentId = request.params[routeParentIdKey],
+					newModel;
+				function createAndSaveNewModel(cb) {
+					var newData = _.extend({parent_id: parentId}, request.payload);
+					newModel = new itemFactory(newData);
+					newModel.save(function(err, newModelResult) {
 						if (handlingError(err, reply)) return;
-						cb(null);
+						cb(null, newModelResult.id);
+					});
+				}
+				function updateParentModelCollection(newModelId, cb) {
+					var parentQuery = {};
+					parentQuery[parentQueryField] = parentId;
+					parentFactory.findOne(parentQuery, function(err, parentModel) {
+						if (handlingError(err, reply)) return;
+						parentModel[parentCollection].push(newModelId);
+						parentModel.save(function(err, parentModel) {
+							if (handlingError(err, reply)) return;
+							cb(null);
+						});
+					});
+				}
+				async.waterfall([createAndSaveNewModel, updateParentModelCollection], function(err) {
+					if (handlingError(err, reply)) return;
+					reply(newModel.toJSON());
+				});
+			},
+			config: {
+				auth: app.authConfig
+			}
+		},
+		update: {
+			handler: function(request, reply) {
+				if (!verifyAuth(request, reply)) return;
+
+				var itemId = request.params[routeItemIdKey] || null
+				treeUpdate(opts, itemId, request.payload, reply);
+			},
+			config: {
+				auth: app.authConfig
+			}
+		},
+		destroy: {
+			handler: function(request, reply) {
+				if (!verifyAuth(request, reply)) return;
+
+				var parentId = request.params[routeParentIdKey];
+				var itemId = request.params[routeItemIdKey];
+				itemFactory.findById(itemId, function(err, itemModel) {  // make sure it exists
+					if (handlingErrorOrMissing(err, itemModel, reply)) return;
+					treeDelete(itemModel, opts, reply, function(err) {
+						if (handlingError(err, reply)) return;
+						reply();
 					});
 				});
+			},
+			config: {
+				auth: app.authConfig
 			}
-			async.waterfall([createAndSaveNewModel, updateParentModelCollection], function(err) {
-				if (handlingError(err, reply)) return;
-				reply(newModel.toJSON());
-			});
-		},
-		update: function(request, reply) {
-			var itemId = request.params[routeItemIdKey] || null
-			treeUpdate(opts, itemId, request.payload, reply);
-		},
-		destroy: function(request, reply) {
-			var parentId = request.params[routeParentIdKey];
-			var itemId = request.params[routeItemIdKey];
-			itemFactory.findById(itemId, function(err, itemModel) {  // make sure it exists
-				if (handlingErrorOrMissing(err, itemModel, reply)) return;
-				treeDelete(itemModel, opts, reply, function(err) {
-					if (handlingError(err, reply)) return;
-					reply();
-				});
-			});
 		}
 	};
 };
