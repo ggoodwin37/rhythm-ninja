3aec920084e4ee717ae3ef7ed6cdbc9761d44478
ggoodwin37 <ggoodwin37@gmail.com>
2015-07-05 14:44:58 -0700

    WIP

---
+++ client/views/pool/pool-entries.js
@@ -0,0 +1,35 @@
+var View = require('ampersand-view');
+var templates = require('../../templates');
+var dom = require('ampersand-dom');
+
+module.exports = View.extend({
+	template: templates.views.set.pool,
+	initialize: function(params) {
+		var self = this;
+
+		this.model = params.model;
+		this.model.on('change:pool', function() {
+			self.render();
+		});
+	},
+	render: function() {
+		var pool = (this.model && this.model.pool) ? this.model.pool : null;
+		this.renderWithTemplate({
+			pool: pool,
+			setName: this.model.name,
+			slugger: function(input) {
+				return input.replace(' ', '-'); // TODO: better slugger
+			}
+		});
+		this.setLoading(!pool);
+	},
+	setLoading: function(isLoading) {
+		var el = this.queryByHook('pool-view-container');
+		var className = 'default-loading';
+		if (isLoading) {
+			dom.addClass(el, className);
+		} else {
+			dom.removeClass(el, className);
+		}
+	}
+});
