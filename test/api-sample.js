c12d2996838728312da5e95c62bf281d9fd1dbb9
gideong <gideong@yahoo-inc.com>
2016-01-25 15:25:52 -0800

    Testing sample api, WIP.

--- test/api-sample.js
+++ test/api-sample.js
@@ -11,6 +11,7 @@ module.exports = function(ctx, lab) {
 	var testAssetPath = 'test/assets/test-wav.wav';
 	var testAssetContentType = 'audio/wav';
 	var testAssetSize = null;
+	var testSampleId = null;
 	describe('verify sample api', () => {
 		it('should allow me to create a new sample', (done) => {
 			fs.readFile(testAssetPath, (err, data) => {
@@ -25,9 +26,34 @@ module.exports = function(ctx, lab) {
 					payload: data
 				}, (res) => {
 					expect(res.statusCode).to.equal(200);
+					expect(res.result.id).to.be.a.string();
+					testSampleId = res.result.id;
 					done();
 				});
 			});
 		});
+		it('should allow me to read the new sample back', (done) => {
+			// TODO
+			done();
+		});
+		it('should allow me to delete the new sample', (done) => {
+			ctx.server.inject({
+				method: 'delete',
+				url: '/api/sample/' + testSampleId
+			}, (res) => {
+				expect(res.statusCode).to.equal(200);
+				done();
+			});
+		});
+		it('should 404 if I try to delete the new sample again', (done) => {
+			ctx.server.inject({
+				method: 'delete',
+				url: '/api/sample/' + testSampleId
+			}, (res) => {
+				expect(res.statusCode).to.equal(404);
+				done();
+			});
+		});
+
 	});
 };
